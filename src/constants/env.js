22146a8e323165c8f1acc7797b49b776802366f0
“MarioDev64” <marioytorres@hotmail.com>
2023-01-30 20:35:33 -0400

    refactor(.env & env.js): setting up enviroment config

--- src/constants/env.js
+++ src/constants/env.js
@@ -3,7 +3,7 @@ let API_URL = "";
 if(process.env.NODE_ENV === "development"){
     API_URL = process.env["REACT_APP_API_URL"];
 }else{
-    API_URL = process.env["REACT_API_URL"];
+    API_URL = process.env["REACT_APP_API_URL"];
 }
 
 export { API_URL }

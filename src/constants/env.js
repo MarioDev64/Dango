24fc6de44ccb476a9284413d8f2a98b721d211d8
“MarioDev64” <marioytorres@hotmail.com>
2023-01-30 20:25:08 -0400

    fix(.env & env.js): add .env file and config needed

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,1 +1,9 @@
-export const API_URL = 'http://localhost:3001';
+let API_URL = "";
+
+if(process.env.NODE_ENV === "development"){
+    API_URL = process.env["REACT_APP_API_URL"];
+}else{
+    API_URL = process.env["REACT_API_URL"];
+}
+
+export { API_URL }

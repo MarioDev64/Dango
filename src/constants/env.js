98f50c6e8f4db8021e3825e556d0cc2657ea8b59
“MarioDev64” <marioytorres@hotmail.com>
2023-01-31 00:43:17 -0400

    fix(env.js): restore default settings

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,9 +1,1 @@
-let API_URL = "";
-
-if(process.env.NODE_ENV === "development"){
-    API_URL = process.env["REACT_APP_API_URL"];
-}else{
-    API_URL = process.env["RAILWAY_STATIC_URL"];
-}
-
-export { API_URL }
+export const API_URL = 'http://localhost:3001';
